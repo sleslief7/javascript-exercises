830302885751e05300b1b6c4d9e0d18c3080c8b0
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-02 12:51:10 -0400

    Deleted ".skip from tests"
    Created repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    for(let i = 1; i <= num; i++) {
+        if(num === 0){
+            repeatedString = '';
+        } else if(num < 0) {
+            repeatedString = 'ERROR';
+        } else {
+            repeatedString += string;
+        }
+        
+    }
+    return repeatedString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;

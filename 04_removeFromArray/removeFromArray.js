3066bb862d78fb6daabaee0c65bdb202394a9157
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-02 13:59:03 -0400

    Final commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...params) {
+    return arr.filter(e => !params.includes(e));
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;

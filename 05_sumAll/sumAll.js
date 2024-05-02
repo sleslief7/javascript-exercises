5a44a6bde36316bc48508fe0c83456d139b1d07b
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-02 15:15:08 -0400

    Final commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if(typeof(start) !== 'number' || typeof(end) !== 'number'){
+        return 'ERROR';
+    }
+    let sum = 0;
+    let bigger = Math.max(start, end);
+    let smaller = Math.min(start, end);
+    for(let i = smaller; i <= bigger; i++){
+        sum += i;
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;

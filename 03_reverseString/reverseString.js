bc05c473b591eea0906e855db88eb8381eaba863
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-02 13:02:23 -0400

    Final commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arrayFromStr = str.split("");
+    return arrayFromStr.reverse().join('');
 };
 
 // Do not edit below this line

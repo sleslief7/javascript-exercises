5d1943021202190a44cf05b2fcd120d0640e52b2
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-08 15:35:35 -0400

    Final commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = position => {
+    position = Number(position);
+    if(position < 0 || isNaN(position)) return 'OOPS';
+    if(position === 0) return 0;
+    if(position === 1 || position === 2) return 1;
 
-};
+
+    let beforeBefore = 1;
+    let before = 1;
+    let current;
+    for(let i = 2; i < position; i++) {
+        current = beforeBefore + before;
+        beforeBefore = before;
+        before = current;
+    }
+    return current;
+}
+
+console.log(fibonacci(5))
 
 // Do not edit below this line
 module.exports = fibonacci;

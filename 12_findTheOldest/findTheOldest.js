aea34156e722995c318934bff8670b48502a3974
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-08 17:08:16 -0400

    Final commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = arr => {
+    let oldestToYoungest = arr.sort((a, b) => {
+        return getPersonAge(b) - getPersonAge(a);
+    });
+    return oldestToYoungest[0];
+}
 
-};
+const getPersonAge = person => {
+    if("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    const currentYear = (new Date()).getFullYear();
+    return currentYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

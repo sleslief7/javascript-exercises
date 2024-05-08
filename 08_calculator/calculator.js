5c84a366f5d15b593c6a3ba78b2d5365b47b6363
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-08 12:42:11 -0400

    Final commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,28 @@
-const add = function() {
+const add = (a, b) => a + b;
 	
-};
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = arr => arr.reduce((acc, num) => acc + num, 0);
 
-const multiply = function() {
+const multiply = arr => arr.reduce((acc, num) => acc * num, 1);
 
-};
+const power = (a, b) => Math.pow(a, b);
 
-const power = function() {
-	
-};
+const factorial = num => {
+  if(num === 0) {
+    return 1;
+  }
+  for(let i = (num - 1); i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
 
-const factorial = function() {
-	
-};
+// const factorialRec = num => {
+//   if(num === 0) return 1;
+//   return num * factorialRec(num-1);
+// }
 
 // Do not edit below this line
 module.exports = {

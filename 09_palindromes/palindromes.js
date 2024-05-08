ec3504c827ce81df760da5993f1094264bcd682e
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-05-08 13:23:11 -0400

    Final commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
-
+const palindromes = str => {
+    let regex = /[^a-z\d]+/gi;
+    str = str.toLowerCase().replace(regex, '')
+    let reverse = [...str].reverse().join('');
+    return reverse === str;
+}
 // Do not edit below this line
 module.exports = palindromes;
